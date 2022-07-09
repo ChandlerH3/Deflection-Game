65b4765e148c443117f53e4d7412445a582f6844
Chandler Huang <71352198+ChandlerH3@users.noreply.github.com>
2022-07-09 20:42:21 -0230

    README update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Deflection-Game
+## Introduction
+
+This project was designed to practice and learn about Object Oriented Programming(OOP). 
+
+This game is called **Deflection-Game**. In the game, you play as Sekiro. You can move up, down, left or right with the arrows of your keyboard, and also you can also tap spacebar to deflect as an attack is coming. This will negate all damage.
+
+The goal of the game is to stay alive as long as possible by avoiding the Charging bulls  The longer you stay alive, the higher your score!
+
+
+
--- js/Enemy.js
+++ js/Enemy.js
@@ -19,55 +19,6 @@ class Enemy {
     this.root = theRoot;
     this.spot = enemySpot;
 
-    // The x position of the enemy is determined by its width and its spot. We need this information for the lifetime
-    // of the instance, so we make it a property of the instance. (Why is this information needed for the lifetime of the instance?)
-  //   this.x = enemySpot * ENEMY_WIDTH;
-
-  //   // The y position is initially less than 0 so that the enemies fall from the top. This data is stored as a property
-  //   // of the instance since it is needed throughout its lifetime. The destroyed property will indicate whether this enemy
-  //   // is still in play. It is set to true whenever the enemy goes past the bottom of the screen.
-  //   // It is used in the Engine to determine whether or not an enemy is in a particular column.
-  //   this.y = -ENEMY_HEIGHT;
-  //   this.destroyed = false;
-
-  //   // We create a new DOM element. The tag of this DOM element is img. It is the DOM node that will display the enemy image
-  //   // to the user. When the enemy is no longer needed, we will use a reference to this DOM node to remove it from the game. This
-  //   // is why we create a property that refers to it.
-  //   this.domElement = document.createElement('img');
-
-  //   // We give it a src attribute to specify which image to display.
-  //   this.domElement.src = 'Bull.png';
-  //   // We modify the CSS style of the DOM node.
-  //   this.domElement.style.position = 'absolute';
-  //   this.domElement.style.left = `${this.x}px`;
-  //   this.domElement.style.top = `${this.y}px`;
-  //   this.domElement.style.zIndex = 5;
-
-  //   // Show that the user can actually see the img DOM node, we append it to the root DOM node.
-  //   theRoot.appendChild(this.domElement);
-  //   this.speed = Math.random() / 2 + 0.25;
-  // }
-
-  // // We set the speed property of the enemy. This determines how fast it moves down the screen.
-  // // To make sure that every enemy has a different speed, we use Math.random()
-  // // this method will be called on the enemy instance every few milliseconds. The parameter
-  // // timeDiff refers to the number of milliseconds since the last update was called.
-  // update(timeDiff) {
-  //   // We update the y property of the instance in proportion of the amount of time
-  //   // since the last call to update. We also update the top css property so that the image
-  //   // is updated on screen
-  //   this.y = this.y + timeDiff * this.speed;
-  //   this.domElement.style.top = `${this.y}px`;
-
-  //   // If the y position of the DOM element is greater than the GAME_HEIGHT then the enemy is at the bottom
-  //   // of the screen and should be removed. We remove the DOM element from the root DOM element and we set
-  //   // the destroyed property to indicate that the enemy should no longer be in play
-  //   if (this.y > GAME_HEIGHT) {
-  //     this.root.removeChild(this.domElement);
-
-  //     this.destroyed = true;
-  //   }
-  // }
   this.y = enemySpot * ENEMY_HEIGHT;
 
   // The y position is initially less than 0 so that the enemies fall from the top. This data is stored as a property
